ad8ce5ee78b72e4f1dc8dff1bb8a2f959fb1f840
salim-hub42 <salim.khalfoun@yahoo.fr>
2026-05-01 11:06:44 +0200

    Ajout du cron job de suppression

---
+++ src/cron-delete-files.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'bddCrud.php';
+$db = connexionBDD();
+
+function deleteFilesDb()
+{
+   $db = connexionBDD();
+   // Durée de vie des enregistrements en secondes (7 jours)
+   $duration = 60 * 60 * 24 * 7;
+   // Date limite de suppression des enregistrements
+   $expiration = time() - $duration;
+
+   // Convertir la date limite en format SQL
+   $expirationDate = date('Y-m-d H:i:s', $expiration);
+
+   // Requête pour supprimer les enregistrements expirés
+   $stmt = $db->prepare("DELETE FROM piece_jointe WHERE date_creation < :expirationDate");
+   $stmt->bindParam(':expirationDate', $expirationDate);
+   $stmt->execute();
+
+   echo "Les enregistrements ont été supprimés avec succès.";
+
+   $handle = fopen('log.txt', 'a+');
+   fwrite($handle, date('Y-m-d H:i:s') . " Les enregistrements ont été supprimés avec succès.\n");
+   fclose($handle);
+}
